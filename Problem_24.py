7e531d9b6ba23de8dfe6cadb7ae390ec4a4e5516
YOGEESH K P <yogeesh793@gmail.com>
2025-01-14 00:11:04 +0530

    Add files via upload

---
+++ Problem_24.py
@@ -0,0 +1,8 @@
+"""Sum of First 10 Numbers:
+
+Write a program using a for loop that calculates the sum of numbers from 1 to 10."""
+
+total=0
+for i in range(1,11):
+    total = total+i
+print(total)
